0ab9305b56d41cb9edc92585e9c1f425b3bacbf2
JosiahElder <@JosiahElder>
2023-04-10 22:42:57 -0700

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.JFrame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Main
+{
+    private static int WIDTH = 600;
+    private static int HEIGHT = 400;
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Ball Game");
+        BallGame game = new BallGame();
+        frame.add(game);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        game.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+
+            }
+        });
     }
 }
