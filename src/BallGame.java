2d724bf97a99feac618ec9f662c906892ace77be
JosiahElder <@JosiahElder>
2023-04-10 23:00:05 -0700

    Next Commit

--- src/BallGame.java
+++ src/BallGame.java
@@ -5,12 +5,15 @@ import javax.swing.JOptionPane;
 
 public class BallGame extends JPanel
 {
-    private int xPosition = WIDTH / 2;
-    private int yPosition = HEIGHT / 2;
-    private int xSpeed = 2;
-    private int score = 0;
     private static int ballSize = 20;
     private static int maxSpeed = 10;
+    private static int width = 600;
+    private static int height = 400;
+
+    private int xPosition = width / 2;
+    private int yPosition = height / 2;
+    private int xSpeed = 2;
+    private int score = 0;
 
     public void paintComponent(Graphics g)
     {
@@ -22,10 +25,10 @@ public class BallGame extends JPanel
     public void moveBall()
     {
         xPosition += xSpeed;
-        if (xPosition <= 0 || xPosition + ballSize >= WIDTH)
+        if (xPosition <= 0 || xPosition + ballSize >= width)
         {
             JOptionPane.showMessageDialog(null,
-                    "GAME OVER! Your score is " + score);
+                    "GAME OVER! Your score is: " + score);
             System.exit(0);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,36 @@ import java.awt.event.MouseEvent;
 
 public class Main
 {
-    private static int WIDTH = 600;
-    private static int HEIGHT = 400;
-
+    private static int width = 600;
+    private static int height = 400;
 
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Ball Game");
         BallGame game = new BallGame();
         frame.add(game);
-        frame.setSize(WIDTH, HEIGHT);
+        frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
         game.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-
+                game.increaseSpeed();
             }
         });
+
+        while (true)
+        {
+            game.moveBall();
+            game.repaint();
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
